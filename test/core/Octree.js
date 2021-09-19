9a6bc72362d34c19b1e028390f77dbbcfc222819
Raoul v. R <vanruesc@users.noreply.github.com>
2021-09-20 00:08:41 +0200

    Update tests

--- test/core/Octree.js
+++ test/core/Octree.js
@@ -7,12 +7,13 @@ const bounds = new Box3(
 	new Vector3(1, 1, 1)
 );
 
+const p = new Vector3();
+const v = new Vector3();
 const keyCoordinates = new Vector3();
-const keyDesign = new KeyDesign(4, 4, 4);
-const levels = Math.max(keyDesign.x, keyDesign.y, keyDesign.z);
 
 test("can be instantiated", t => {
 
+	const keyDesign = new KeyDesign(1, 1, 1);
 	const octree = new Octree(bounds, keyDesign);
 	t.pass();
 
@@ -20,14 +21,92 @@ test("can be instantiated", t => {
 
 test("can return its depth", t => {
 
+	const keyDesign = new KeyDesign(32, 0, 21);
+	const octree = new Octree(bounds, keyDesign);
+	const levels = Math.max(keyDesign.x, keyDesign.y, keyDesign.z);
+	t.is(octree.getDepth(), levels - 1, "should return the tree depth");
+
+});
+
+test("correctly calculates key coordinates", t => {
+
+	const keyDesign = new KeyDesign(21, 11, 21);
 	const octree = new Octree(bounds, keyDesign);
 
-	t.is(octree.getDepth(), levels - 1, "should return the tree depth");
+	p.set(1, 1, 1);
+	keyDesign.getMaxKeyCoordinates(v);
+	octree.calculateKeyCoordinates(p, 0, keyCoordinates);
+
+	t.deepEqual(keyCoordinates, v,
+		"should return the max key coordinates");
+
+	t.is(keyDesign.packKey(keyCoordinates), keyDesign.packKey(v),
+		"should return the last key");
+
+	p.set(0, 0, 0);
+	v.divideScalar(2).round();
+	octree.calculateKeyCoordinates(p, 0, keyCoordinates);
+
+	t.deepEqual(keyCoordinates, v,
+		"should return the center (+0.5) key coordinates");
+
+	t.is(keyDesign.packKey(keyCoordinates), keyDesign.packKey(v),
+		"should return the center (+0.5) key");
+
+});
+
+test("can set and get data", t => {
+
+	const keyDesign = new KeyDesign(2, 2, 2);
+	const octree = new Octree(bounds, keyDesign);
+	const data = "text";
+
+	p.set(0.5, 0, 0);
+	octree.calculateKeyCoordinates(p, 0, keyCoordinates);
+	octree.set(keyCoordinates, 0, data);
+
+	t.is(octree.get(keyCoordinates, 0), data, "should return the data");
+
+});
+
+test("can delete data", t => {
+
+	const keyDesign = new KeyDesign(2, 2, 2);
+	const octree = new Octree(bounds, keyDesign);
+	const data = "text";
+
+	p.set(0.5, 0, 0);
+	octree.calculateKeyCoordinates(p, 0, keyCoordinates);
+	octree.set(keyCoordinates, 0, data);
+	octree.delete(keyCoordinates, 0, data);
+
+	t.not(octree.get(keyCoordinates, 0), data, "should no longer have the data");
+
+});
+
+test("can get an octant by point", t => {
+
+	const keyDesign = new KeyDesign(2, 2, 2);
+	const octree = new Octree(bounds, keyDesign);
+	const data = "text";
+
+	p.set(0, 0, 0);
+	octree.calculateKeyCoordinates(p, 0, keyCoordinates);
+	octree.set(keyCoordinates, 0, null);
+
+	p.set(1, 0.1, -0.7);
+	octree.calculateKeyCoordinates(p, 0, keyCoordinates);
+	octree.set(keyCoordinates, 0, data);
+
+	const octant = octree.getOctantByPoint(p, 0);
+
+	t.is(octant.data, data, "should find the right octant");
 
 });
 
 test("finds nodes by depth level", t => {
 
+	const keyDesign = new KeyDesign(2, 2, 2);
 	const octree = new Octree(bounds, keyDesign);
 	octree.set(keyCoordinates.set(0, 0, 0), 0, "test");
 
--- test/core/OctreeRaycaster.js
+++ test/core/OctreeRaycaster.js
@@ -9,25 +9,22 @@ const bounds = new Box3(
 
 const position = new Vector3();
 const keyCoordinates = new Vector3();
-const keyDesign = new KeyDesign(4, 4, 4);
+const keyDesign = new KeyDesign(1, 1, 1);
 
 test("can find intersecting octants", t => {
 
 	const octree = new Octree(bounds, keyDesign);
 	const raycaster = new Raycaster(
-		new Vector3(0, 0, -1),
-		new Vector3(0, 0, 0)
+		new Vector3(0, 0, 2),
+		new Vector3(0, 0, -1)
 	);
 
-	octree.calculateKeyCoordinates(position.set(0, 0, 0), 0, keyCoordinates);
+	position.set(0, 0, 0);
+	octree.calculateKeyCoordinates(position, 0, keyCoordinates);
 	octree.set(keyCoordinates, 0, "test");
-
-	console.log(keyCoordinates);
 
 	const result = octree.getIntersectingNodes(raycaster);
 
-	// @todo
-	//t.is(result.length, 1, "should return one intersecting octant");
-	t.pass();
+	t.is(result.length, 1, "should return one intersecting octant");
 
 });
