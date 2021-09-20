f5c0964ce64a7c596bcfec2b43f26f6278760d86
Raoul v. R <vanruesc@users.noreply.github.com>
2021-09-20 15:14:53 +0200

    Update OctreeRaycaster.js

--- test/core/OctreeRaycaster.js
+++ test/core/OctreeRaycaster.js
@@ -15,10 +15,8 @@ test("can find intersecting octants", t => {
 	const keyDesign = new KeyDesign(1, 1, 1);
 	const octree = new Octree(bounds, keyDesign);
 	const raycaster = new Raycaster(
-		// new Vector3(0.5, 0.5, 2),
-		// new Vector3(0, 0, -1)
-		new Vector3(0.5, 0.5, 2),
-		new Vector3(1e-6, 0, -1) // Perfectly aligned direction = miss
+		 new Vector3(0.5, 0.5, 2),
+		 new Vector3(0, 0, -1)
 	);
 
 	p.set(0.5, 0.5, 0.5);
