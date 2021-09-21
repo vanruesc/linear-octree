41ac50eaa8cc08809c45d35059bb72b958cd1693
Raoul v. R <vanruesc@users.noreply.github.com>
2021-09-22 01:19:25 +0200

    Export layout from sparse-octree

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export * from "./core";
 export * from "./raycasting";
 export * from "./utils";
 
-// Re-export interfaces to avoid peer-dependency.
-export { DataContainer, Node, Tree } from "sparse-octree";
+export { layout, DataContainer, Node, Tree } from "sparse-octree";
